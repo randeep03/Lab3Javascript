43e96c2dddb33ea4f46027e45a0a393682f15760
randeep03 <randeepxoo7@gmail.com>
2025-08-11 13:26:21 -0400

    Step  is : Add DOMContentLoaded event listener

---
+++ js/page-color-changer.js
@@ -0,0 +1,4 @@
+// Wait until the DOM is fully loaded because the script is loaded in <head>
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('DOM fully loaded and parsed');
+});
